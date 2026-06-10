b44e4e1f4d3f36a5dfcdc0c614cbb2907d356c05
vvjenterprise13-arch <vvjenterprise13@gmail.com>
2026-06-11 02:42:31 +0530

    Update remove_from_cart.php

--- remove_from_cart.php
+++ remove_from_cart.php
@@ -1,15 +1,13 @@
 <?php
+include('database/connection.php');
 session_start();
 
 if (isset($_GET['pid'])) {
     $product_id_to_remove = (int)$_GET['pid'];
-
-   
     if (isset($_SESSION['cart'][$product_id_to_remove])) {
         unset($_SESSION['cart'][$product_id_to_remove]);
     }
 }
-
 
 header('Location: cart');
 exit();
