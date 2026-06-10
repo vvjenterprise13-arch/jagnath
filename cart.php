5b5eab9ac1ef2afab67045630d5ec0865157f11c
vvjenterprise13-arch <vvjenterprise13@gmail.com>
2026-06-11 02:46:38 +0530

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,7 +1,8 @@
 <?php
+include('database/connection.php');
 session_start();
 
-include('database/connection.php');
+
 
 function get_cached_query_result($conn, $sql, $types, $params, $cache_file = null, $ttl = null) {
     if (!$conn) return [];
